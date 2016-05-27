2aa849d13c13d9c53e9b2f6c704f8e9431c402cd
root <root@vps247038.ovh.net>
2016-05-27 17:44:50 +0200

    Ajout owncloud-config

---
+++ owncloud-config/config.php
@@ -0,0 +1,16 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc0qorscthxw',
+  'passwordsalt' => 'pypbp4J8is3mHif2D3wWOntRPufQ9C',
+  'secret' => 'mSFQjSsFtm9WDiI+2LX8NIjhvW90iClTk0AIe6HiDxyFmCzL',
+  'trusted_domains' => 
+  array (
+    0 => 'owncloud.wt4.ephec-ti.be',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://owncloud.wt4.ephec-ti.be',
+  'dbtype' => 'sqlite3',
+  'version' => '8.1.8.2',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
